f8daab9029a9dbddb548f8df080d6a0fd8d51f72
Josef Kudera <46950237+kudj@users.noreply.github.com>
2026-01-16 11:41:24 +0100

    ruff, removed old docstring

--- src/component.py
+++ src/component.py
@@ -17,9 +17,9 @@ KEY_MODE = "mode"
 KEY_IN_TABLES = "input"
 KEY_QUERIES = "queries"
 KEY_OUT_TABLES = "output"
-DUCK_DB_DIR = os.path.join(os.environ.get('TMPDIR', '/tmp'), 'duckdb')
+DUCK_DB_DIR = os.path.join(os.environ.get("TMPDIR", "/tmp"), "duckdb")
 
-KEY_DEBUG = 'debug'
+KEY_DEBUG = "debug"
 MANDATORY_PARS = []
 
 
@@ -44,7 +44,6 @@ class OutTableParams:
 
 
 class Component(ComponentBase):
-
     def __init__(self):
         ComponentBase.__init__(self)
         self.validate_configuration_parameters(MANDATORY_PARS)
@@ -53,22 +52,18 @@ class Component(ComponentBase):
         self._in_tables = self.get_input_tables_definitions()
 
     def run(self):
-
-        if self._config.get(KEY_MODE) == 'advanced':
+        if self._config.get(KEY_MODE) == "advanced":
             self.advanced_mode()
         else:
             self.simple_mode()
 
     def init_connection(self) -> duckdb.DuckDBPyConnection:
         """
-                Returns connection to temporary DuckDB database
-                """
+        Returns connection to temporary DuckDB database
+        """
         os.makedirs(DUCK_DB_DIR, exist_ok=True)
         # TODO: On GCP consider changin tmp to /opt/tmp
-        config = dict(temp_directory=DUCK_DB_DIR,
-                      threads="4",
-                      memory_limit="512MB",
-                      max_memory="512MB")
+        config = dict(temp_directory=DUCK_DB_DIR, threads="4", memory_limit="512MB", max_memory="512MB")
         conn = duckdb.connect(config=config)
 
         return conn
@@ -86,7 +81,7 @@ class Component(ComponentBase):
             self.run_simple_query(q)
 
             if isinstance(q[KEY_IN_TABLES], dict):
-                pattern = q[KEY_IN_TABLES].get('input_pattern')
+                pattern = q[KEY_IN_TABLES].get("input_pattern")
             else:
                 pattern = q[KEY_IN_TABLES]
 
@@ -115,7 +110,7 @@ class Component(ComponentBase):
 
             pattern = t
             if isinstance(t, dict):
-                pattern = t.get('input_pattern')
+                pattern = t.get("input_pattern")
 
             for table in self._in_tables:
                 if fnmatch.fnmatch(table.name, pattern):
@@ -133,18 +128,21 @@ class Component(ComponentBase):
             table_params = self.get_out_table_params(table)
 
             table_meta = self._connection.execute(f"""DESCRIBE TABLE '{table_params.table_name}';""").fetchall()
-            schema = OrderedDict((c[0], ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1]))))
-                                 for c in table_meta)
+            schema = OrderedDict(
+                (c[0], ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1])))) for c in table_meta
+            )
 
             tm = TableMetadata()
             tm.add_column_data_types({c[0]: self.convert_base_types(c[1]) for c in table_meta})
 
-            out_table = self.create_out_table_definition(f"{table_params.table_name}.csv", schema=schema,
-                                                         primary_key=table_params.primary_key,
-                                                         incremental=table_params.incremental,
-                                                         destination=table_params.destination,
-                                                         table_metadata=tm
-                                                         )
+            out_table = self.create_out_table_definition(
+                f"{table_params.table_name}.csv",
+                schema=schema,
+                primary_key=table_params.primary_key,
+                incremental=table_params.incremental,
+                destination=table_params.destination,
+                table_metadata=tm,
+            )
 
             try:
                 self._connection.execute(f'''COPY "{table_params.table_name}" TO "{out_table.full_path}"
@@ -164,12 +162,13 @@ class Component(ComponentBase):
         primary_key = []
 
         if isinstance(q.get(KEY_OUT_TABLES), dict):
-            incremental = q[KEY_OUT_TABLES].get('incremental', False)
-            primary_key = q[KEY_OUT_TABLES].get('primary_key', [])
+            incremental = q[KEY_OUT_TABLES].get("incremental", False)
+            primary_key = q[KEY_OUT_TABLES].get("primary_key", [])
 
         table_meta = self._connection.execute(f"""DESCRIBE {q["query"]};""").fetchall()
-        schema = OrderedDict((c[0], ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1]))))
-                             for c in table_meta)
+        schema = OrderedDict(
+            (c[0], ColumnDefinition(data_types=BaseType(dtype=self.convert_base_types(c[1])))) for c in table_meta
+        )
 
         tm = TableMetadata()
         tm.add_column_data_types({c[0]: self.convert_base_types(c[1]) for c in table_meta})
@@ -177,7 +176,7 @@ class Component(ComponentBase):
         input_data = q.get(KEY_IN_TABLES)
         output_data = q.get(KEY_OUT_TABLES)
         if isinstance(input_data, dict):
-            destination = input_data.get('duckdb_destination') or input_data.get("input_pattern")
+            destination = input_data.get("duckdb_destination") or input_data.get("input_pattern")
         else:
             destination = input_data
 
@@ -186,25 +185,27 @@ class Component(ComponentBase):
         elif isinstance(output_data, str):
             destination = output_data
 
-        table_name = f"{destination.replace(".csv", "")}.csv"
+        table_name = f"{destination.replace('.csv', '')}.csv"
 
-        out_table = self.create_out_table_definition(table_name,
-                                                     schema=schema,
-                                                     primary_key=primary_key,
-                                                     incremental=incremental,
-                                                     destination=destination,
-                                                     table_metadata=tm
-                                                     )
+        out_table = self.create_out_table_definition(
+            table_name,
+            schema=schema,
+            primary_key=primary_key,
+            incremental=incremental,
+            destination=destination,
+            table_metadata=tm,
+        )
 
         try:
-            self._connection.execute(f'COPY ({q["query"]}) TO "{out_table.full_path}"'
-                                     f'(HEADER, DELIMITER ",", FORCE_QUOTE *)')
+            self._connection.execute(
+                f'COPY ({q["query"]}) TO "{out_table.full_path}"(HEADER, DELIMITER ",", FORCE_QUOTE *)'
+            )
         except duckdb.duckdb.ConversionException as e:
             raise UserException(f"Error during query execution: {e}")
 
         self.write_manifest(out_table)
 
-        logging.debug(f'Table {table_name} export finished.')
+        logging.debug(f"Table {table_name} export finished.")
 
     def _get_table_header(self, t: TableDefinition):
         """
@@ -213,16 +214,16 @@ class Component(ComponentBase):
         if t.is_sliced or t.column_names:
             header = t.column_names
         else:
-            with open(t.full_path, encoding='utf-8') as input:
+            with open(t.full_path, encoding="utf-8") as input:
                 delimiter = t.delimiter
                 enclosure = t.enclosure
-                reader = DictReader(input, lineterminator='\n', delimiter=delimiter, quotechar=enclosure)
+                reader = DictReader(input, lineterminator="\n", delimiter=delimiter, quotechar=enclosure)
                 header = reader.fieldnames
 
         return header
 
     def _has_header_in_file(self, t: TableDefinition):
-        is_input_mapping_manifest = t.stage == 'in'
+        is_input_mapping_manifest = t.stage == "in"
         if t.is_sliced:
             has_header = False
         elif t.column_names and not is_input_mapping_manifest:
@@ -238,20 +239,29 @@ class Component(ComponentBase):
         if delimiter is None:
             delimiter = table_params.table.delimiter if table_params.table else ","
 
-        quote_char = table_params.table_config.get('quotechar')
-        has_header = table_params.table_config.get('has_header') or table_params.has_header
-        header = table_params.table_config.get('column_names') or table_params.header or None
-        skip = table_params.table_config.get('skip_lines')
-        date_format = table_params.table_config.get('date_format')
-        timestamp_format = table_params.table_config.get('timestamp_format')
-        add_filename_col = table_params.table_config.get('add_filename_column')
+        quote_char = table_params.table_config.get("quotechar")
+        has_header = table_params.table_config.get("has_header") or table_params.has_header
+        header = table_params.table_config.get("column_names") or table_params.header or None
+        skip = table_params.table_config.get("skip_lines")
+        date_format = table_params.table_config.get("date_format")
+        timestamp_format = table_params.table_config.get("timestamp_format")
+        add_filename_col = table_params.table_config.get("add_filename_column")
 
         # dynamically name the relation as a table name so it can be accessed later from the query
         try:
             globals()[table_params.destination_table_name] = self._connection.read_csv(
-                path_or_buffer=table_params.path, delimiter=delimiter, quotechar=quote_char, header=has_header,
-                names=header, skiprows=skip, date_format=date_format, timestamp_format=timestamp_format,
-                filename=add_filename_col, dtype=table_params.dtypes, all_varchar=table_params.all_varchar)
+                path_or_buffer=table_params.path,
+                delimiter=delimiter,
+                quotechar=quote_char,
+                header=has_header,
+                names=header,
+                skiprows=skip,
+                date_format=date_format,
+                timestamp_format=timestamp_format,
+                filename=add_filename_col,
+                dtype=table_params.dtypes,
+                all_varchar=table_params.all_varchar,
+            )
 
             logging.debug(f"Table {table_params.destination_table_name} created.")
         except duckdb.duckdb.IOException:
@@ -264,25 +274,25 @@ class Component(ComponentBase):
         all_varchar = False
 
         if isinstance(table_config, str):
-            table_config = {'input_pattern': table_config}
+            table_config = {"input_pattern": table_config}
 
         if any(char in table_config.get("input_pattern") for char in "*?["):
-            if not table_config.get('duckdb_destination'):
+            if not table_config.get("duckdb_destination"):
                 raise UserException("Destination must be set if input path contains pattern.")
             destination_table_name = table_config["duckdb_destination"]
             path = table_config["input_pattern"]
             has_header = False
             header = False
-            dtypes_param = table_config.get('dtypes_mode')
+            dtypes_param = table_config.get("dtypes_mode")
 
             matched_tables = [i for i in self._in_tables if fnmatch.fnmatch(i.name, table_config["input_pattern"])]
-            if dtypes_param == 'from_manifest':
+            if dtypes_param == "from_manifest":
                 dtypes = {key: value.data_types.get("base").dtype for key, value in matched_tables[0].schema.items()}
-            elif dtypes_param == 'all_varchar':
+            elif dtypes_param == "all_varchar":
                 all_varchar = True
 
         else:
-            destination_table_name = table_config.get('duckdb_destination') or table_config["input_pattern"]
+            destination_table_name = table_config.get("duckdb_destination") or table_config["input_pattern"]
             matched_tables = [i for i in self._in_tables if i.name == table_config["input_pattern"]]
 
             if len(matched_tables) == 0:
@@ -291,18 +301,18 @@ class Component(ComponentBase):
                 raise UserException(f"Multiple input tables with name {table_config['input_pattern']} found.")
 
             table = matched_tables[0]
-            header = table_config.get('column_names') or self._get_table_header(table)
+            header = table_config.get("column_names") or self._get_table_header(table)
             has_header = self._has_header_in_file(table)
 
-            dtypes_param = table_config.get('dtypes_mode')
+            dtypes_param = table_config.get("dtypes_mode")
 
-            if dtypes_param == 'from_manifest':
+            if dtypes_param == "from_manifest":
                 dtypes = {key: value.data_types.get("base").dtype for key, value in table.schema.items()}
-            elif dtypes_param == 'all_varchar':
+            elif dtypes_param == "all_varchar":
                 all_varchar = True
 
             if table.is_sliced:
-                path = f'{table.full_path}/*.csv'
+                path = f"{table.full_path}/*.csv"
             else:
                 path = table.full_path
 
@@ -310,18 +320,18 @@ class Component(ComponentBase):
 
     def get_out_table_params(self, out_table_config: Union[dict, str]) -> OutTableParams:
         if isinstance(out_table_config, dict):
-            source = out_table_config.get('duckdb_source')
+            source = out_table_config.get("duckdb_source")
             if not source:
                 raise ValueError("Missing source in out_tables definition")
-            incremental = out_table_config.get('incremental', False)
-            primary_key = out_table_config.get('primary_key', [])
-            destination = out_table_config.get('kbc_destination')
+            incremental = out_table_config.get("incremental", False)
+            primary_key = out_table_config.get("primary_key", [])
+            destination = out_table_config.get("kbc_destination")
             table_name = source.replace(".csv", "")
 
         else:
             incremental = False
             primary_key = []
-            destination = ''
+            destination = ""
             table_name = out_table_config.replace(".csv", "")
 
         return OutTableParams(table_name, destination, incremental, primary_key)
@@ -340,36 +350,46 @@ class Component(ComponentBase):
             shutil.copy(source.full_path, destination.full_path)
         elif os.path.isdir(source.full_path):
             shutil.copytree(source.full_path, destination.full_path, dirs_exist_ok=True)
-        if Path(f'{source.full_path}.manifest').exists():
-            shutil.copy(f'{source.full_path}.manifest', f'{destination.full_path}.manifest')
+        if Path(f"{source.full_path}.manifest").exists():
+            shutil.copy(f"{source.full_path}.manifest", f"{destination.full_path}.manifest")
         else:
             self.write_manifest(destination)
 
     def convert_base_types(self, dtype: str) -> SupportedDataTypes:
-        if dtype in ['TINYINT', 'SMALLINT', 'INTEGER', 'BIGINT', 'HUGEINT',
-                     'UTINYINT', 'USMALLINT', 'UINTEGER', 'UBIGINT', 'UHUGEINT']:
+        if dtype in [
+            "TINYINT",
+            "SMALLINT",
+            "INTEGER",
+            "BIGINT",
+            "HUGEINT",
+            "UTINYINT",
+            "USMALLINT",
+            "UINTEGER",
+            "UBIGINT",
+            "UHUGEINT",
+        ]:
             return SupportedDataTypes.INTEGER
-        elif dtype in ['REAL', 'DECIMAL']:
+        elif dtype in ["REAL", "DECIMAL"]:
             return SupportedDataTypes.NUMERIC
-        elif dtype == 'DOUBLE':
+        elif dtype == "DOUBLE":
             return SupportedDataTypes.FLOAT
-        elif dtype == 'BOOLEAN':
+        elif dtype == "BOOLEAN":
             return SupportedDataTypes.BOOLEAN
-        elif dtype in ['TIMESTAMP', 'TIMESTAMP WITH TIME ZONE']:
+        elif dtype in ["TIMESTAMP", "TIMESTAMP WITH TIME ZONE"]:
             return SupportedDataTypes.TIMESTAMP
-        elif dtype == 'DATE':
+        elif dtype == "DATE":
             return SupportedDataTypes.DATE
         else:
             return SupportedDataTypes.STRING
 
     def _copy_manifest_to_out(self, t: TableDefinition):
         if t.get_manifest_dictionary():
-            new_path = os.path.join(self.tables_out_path, Path(t.full_path).name + '.manifest')
-            shutil.copy(t.full_path + '.manifest', new_path)
+            new_path = os.path.join(self.tables_out_path, Path(t.full_path).name + ".manifest")
+            shutil.copy(t.full_path + ".manifest", new_path)
 
     def add_column_to_manifest_and_copy_to_out(self, file_path, manifest, new_column):
-        manifest['columns'].append(new_column)
-        with open(os.path.join(self.tables_out_path, Path(file_path).name + '.manifest'), 'w+') as out_f:
+        manifest["columns"].append(new_column)
+        with open(os.path.join(self.tables_out_path, Path(file_path).name + ".manifest"), "w+") as out_f:
             json.dump(manifest, out_f)
 
     def _copy_table_to_out(self, t: TableDefinition):
